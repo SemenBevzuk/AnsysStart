8f8bb1d8866b1d355b82a7d6d3536231e6b3d68d
Semen Bevzuk <semen.bevzuk@gmail.com>
2018-02-22 21:38:40 +0300

    Минимальная реализация для запуска

---
+++ AnsysStart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ansys.Interop.ANSYSWB;
+
+namespace AnsysStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wb = new WBClass();
+            //wb.StartApplet = "AAOApplet"; 
+            wb.Run();
+        }
+    }
+}
